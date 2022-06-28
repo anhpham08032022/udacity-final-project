8e3b12b835d51253901a1cd93dc11da391e2b4bc
anh.pham <anh.pham081188@gmail.com>
2022-06-28 21:07:14 +0700

    implemented notification demo

--- backend/src/lambda/scheduler/filterWarningMessage.ts
+++ backend/src/lambda/scheduler/filterWarningMessage.ts
@@ -4,16 +4,26 @@ const snsWarningTopic = process.env.SNS_WARNING_TOPIC
 const accountId = process.env.AWS_ACCOUNT_ID
 const region = process.env.REGION
 exports.handler = function() {
+    AWS.config.update({region: region})
     console.log
     const topicArn = "arn:aws:sns:" + region + ":" + accountId + ":" + snsWarningTopic
     console.log(`ARN of Topic: ${topicArn}`)
     var eventText = "Hello SNS"
     console.log("Received event:", eventText);
-    var sns = new AWS.SNS();
     var params = {
         Message: eventText, 
         Subject: "Test SNS From Lambda",
         TopicArn: topicArn
     };
-    sns.publish(params);
+    var publishTextPromise = new AWS.SNS({apiVersion: '2010-03-31'}).publish(params).promise();
+
+    // Handle promise's fulfilled/rejected states
+    publishTextPromise.then(
+    function(data) {
+        console.log(`Message ${params.Message} sent to the topic ${params.TopicArn}`);
+        console.log("MessageID is " + data.MessageId);
+    }).catch(
+        function(err) {
+        console.error(err, err.stack);
+    });
 };
