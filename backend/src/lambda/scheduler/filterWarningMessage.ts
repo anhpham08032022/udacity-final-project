899bcb5ef2a156b90dc2820919870472c4919ddf
anh.pham <anh.pham081188@gmail.com>
2022-06-28 22:47:29 +0700

    one day left

--- backend/src/lambda/scheduler/filterWarningMessage.ts
+++ backend/src/lambda/scheduler/filterWarningMessage.ts
@@ -5,28 +5,37 @@ const snsWarningTopic = process.env.SNS_WARNING_TOPIC
 const accountId = process.env.AWS_ACCOUNT_ID
 const region = process.env.REGION
 exports.handler = async function() {
-    AWS.config.update({region: region})
-    const topicArn = "arn:aws:sns:" + region + ":" + accountId + ":" + snsWarningTopic
+    AWS.config.update({region: region});
+    const topicArn = "arn:aws:sns:" + region + ":" + accountId + ":" + snsWarningTopic;
     var todoItems = await filterDoneIsFalse();
     console.log("Received todo items:", todoItems);
     todoItems.forEach(item => {
-        let msg = `Hi ${item.userId}. Your ${item.name} has one day left, please proceed.`
-        var params = {
-            Message: msg, 
-            Subject: "List of undone Todos",
-            TopicArn: topicArn
-        };
-        var publishTextPromise = new AWS.SNS({apiVersion: '2010-03-31'}).publish(params).promise();
+        if (isOneDayLeft(item.dueDate)) {
+            let msg = `Hi ${item.userId}. Your ${item.name} has one day left, please proceed.`;
+            console.log(`Send notification with message: ${msg}`)
+            var params = {
+                Message: msg, 
+                Subject: "List of undone Todos",
+                TopicArn: topicArn
+            };
+            var publishTextPromise = new AWS.SNS({apiVersion: '2010-03-31'}).publish(params).promise();
 
-        // Handle promise's fulfilled/rejected states
-        publishTextPromise.then(
-        function(data) {
-            console.log(`Message ${params.Message} sent to the topic ${params.TopicArn}`);
-            console.log("MessageID is " + data.MessageId);
-        }).catch(
-            function(err) {
-            console.error(err, err.stack);
-        });
-    })
-    
+            publishTextPromise.then(
+            function(data) {
+                console.log(`Message ${params.Message} sent to the topic ${params.TopicArn}`);
+                console.log("MessageID is " + data.MessageId);
+            }).catch(
+                function(err) {
+                console.error(err, err.stack);
+            });
+        }
+    });
 };
+
+function isOneDayLeft(compareDate:string): boolean {
+    var now = new Date();
+    var date = new Date(compareDate);
+    var diff = date.getTime() - now.getTime();
+    var dayDiff = diff / (1000 * 60 * 60 * 24);
+    return (dayDiff > 0 && dayDiff <= 1);
+}
